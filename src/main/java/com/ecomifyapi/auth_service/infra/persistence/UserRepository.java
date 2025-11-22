b09bbd5583a7274f9dd72016c08273035df987f2
joaopedrooliveir4 <jp.oliveira04@hotmail.com>
2025-11-21 21:17:04 -0300

    adiciona UserRepository estendendo JpaRepository para operações de persistência de usuários

---
+++ src/main/java/com/ecomifyapi/auth_service/infra/persistence/UserRepository.java
@@ -0,0 +1,7 @@
+package com.ecomifyapi.auth_service.infra.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+}
