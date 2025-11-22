93de302db67bc80851746500d5420c5e81578466
joaopedrooliveir4 <jp.oliveira04@hotmail.com>
2025-11-21 21:17:52 -0300

    estrutura do projeto

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50),
+    created_at TIMESTAMP
+);
