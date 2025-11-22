be84ffa5506d94cd30bd162bee2dfc82f8ba8393
joaopedrooliveir4 <jp.oliveira04@hotmail.com>
2025-11-21 21:13:55 -0300

    define entidade User com validações, dados básicos, função de promoção e atributos padrão

---
+++ src/main/java/com/ecomifyapi/auth_service/domain/entities/User.java
@@ -0,0 +1,71 @@
+package com.ecomifyapi.auth_service.domain.entities;
+
+import com.ecomifyapi.auth_service.domain.enums.Role;
+
+import java.time.LocalDateTime;
+
+public class User {
+    private String name;
+    private final String email;
+    private String password;
+    private Role role;
+    private LocalDateTime createdAt;
+
+    public User(String name, String email, String password) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Name não pode ser vazio");
+        }
+        if (email == null || !email.contains("@")) {
+            throw new IllegalArgumentException("Email inválido");
+        }
+        if (password == null || password.length() < 6) {
+            throw new IllegalArgumentException("Senha deve ter pelo menos 6 caracteres");
+        }
+
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = Role.USER; // sempre começa como USER
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public void promoteToAdmin() {
+        this.role = Role.ADMIN;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
