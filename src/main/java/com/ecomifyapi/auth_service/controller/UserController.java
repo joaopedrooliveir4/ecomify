1714f8010ed806a87dcae90a8eddc7e61bd5b228
joaopedrooliveir4 <jp.oliveira04@hotmail.com>
2025-11-30 15:04:54 -0300

    ajustar endpoints de atualização de senha e deleção de usuário

--- src/main/java/com/ecomifyapi/auth_service/controller/UserController.java
+++ src/main/java/com/ecomifyapi/auth_service/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.ecomifyapi.auth_service.controller;
 
 import com.ecomifyapi.auth_service.application.usecase.CretedUser;
+import com.ecomifyapi.auth_service.application.usecase.DeleteUser;
+import com.ecomifyapi.auth_service.application.usecase.UpdatePassword;
 import com.ecomifyapi.auth_service.application.usecase.UpdateUserRole;
 import com.ecomifyapi.auth_service.controller.dtos.UserRequest;
 import com.ecomifyapi.auth_service.controller.dtos.UserResponse;
+import com.ecomifyapi.auth_service.controller.integrationDTO.UpdatePasswordRequest;
 import com.ecomifyapi.auth_service.controller.mapper.UserDtoMapper;
 import com.ecomifyapi.auth_service.controller.producer.AuthProducer;
 import com.ecomifyapi.auth_service.domain.entities.User;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,12 +20,16 @@ public class UserController {
     private final UpdateUserRole updateUserRole;
     private final UserDtoMapper userDtoMapper;
     private final AuthProducer authProducer;
+    private final UpdatePassword updatePassword;
+    private final DeleteUser deleteUser;
 
-    public UserController(CretedUser cretedUser, UpdateUserRole updateUserRole, UserDtoMapper userDtoMapper, AuthProducer authProducer) {
+    public UserController(CretedUser cretedUser, UpdateUserRole updateUserRole, UserDtoMapper userDtoMapper, AuthProducer authProducer, UpdatePassword updatePassword, DeleteUser deleteUser) {
         this.cretedUser = cretedUser;
         this.updateUserRole = updateUserRole;
         this.userDtoMapper = userDtoMapper;
         this.authProducer = authProducer;
+        this.updatePassword = updatePassword;
+        this.deleteUser = deleteUser;
     }
 
     @PostMapping
@@ -34,8 +42,20 @@ public class UserController {
         return userDtoMapper.toResponse(user);
     }
 
-    @PutMapping("update/{id}")
-    public User update(@RequestBody User user, @PathVariable Long id) {
+    @PutMapping("update/role/{id}")
+    public User updateRole(@RequestBody User user, @PathVariable Long id) {
         return updateUserRole.update(user, id);
+    }
+
+    @PutMapping("update/password/{id}")
+    public User updatePassword(@RequestBody UpdatePasswordRequest request,
+                               @PathVariable Long id) {
+        return updatePassword.update(request.password(), id);
+    }
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<Void> deleteUser (@PathVariable Long id) {
+        deleteUser.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
