e3a2221cad485b31ed7d73558ac056740cd92b24
joaopedrooliveir4 <jp.oliveira04@hotmail.com>
2025-11-30 14:01:08 -0300

    adicionando método put

--- src/main/java/com/ecomifyapi/auth_service/controller/UserController.java
+++ src/main/java/com/ecomifyapi/auth_service/controller/UserController.java
@@ -1,25 +1,25 @@
 package com.ecomifyapi.auth_service.controller;
 
 import com.ecomifyapi.auth_service.application.usecase.CretedUser;
+import com.ecomifyapi.auth_service.application.usecase.UpdateUserRole;
 import com.ecomifyapi.auth_service.controller.dtos.UserRequest;
 import com.ecomifyapi.auth_service.controller.dtos.UserResponse;
 import com.ecomifyapi.auth_service.controller.mapper.UserDtoMapper;
 import com.ecomifyapi.auth_service.controller.producer.AuthProducer;
 import com.ecomifyapi.auth_service.domain.entities.User;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("ecomifyapi/user")
 public class UserController {
     private final CretedUser cretedUser;
+    private final UpdateUserRole updateUserRole;
     private final UserDtoMapper userDtoMapper;
     private final AuthProducer authProducer;
 
-    public UserController(CretedUser cretedUser, UserDtoMapper userDtoMapper, AuthProducer authProducer) {
+    public UserController(CretedUser cretedUser, UpdateUserRole updateUserRole, UserDtoMapper userDtoMapper, AuthProducer authProducer) {
         this.cretedUser = cretedUser;
+        this.updateUserRole = updateUserRole;
         this.userDtoMapper = userDtoMapper;
         this.authProducer = authProducer;
     }
@@ -32,5 +32,10 @@ public class UserController {
         authProducer.integrar(user);
 
         return userDtoMapper.toResponse(user);
+    }
+
+    @PutMapping("update/{id}")
+    public User update(@RequestBody User user, @PathVariable Long id) {
+        return updateUserRole.update(user, id);
     }
 }
